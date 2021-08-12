97317a9d05bc2b4faf149c6d5d74ec425ac6b6c6
Artur <73757154+Ex6linz@users.noreply.github.com>
2021-08-12 17:32:45 +0200

    Update Users.java

--- Users.java
+++ Users.java
@@ -2,14 +2,25 @@ import java.beans.Statement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class Users {
     public static void user() {
-       // Connection connect = DriverManager.getConnection();
-       // Statement state = connect.createStatement();
-       // ResultSet result = state.execute();
+        try {
+            Connection connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/log1", "root", "123");
+            Statement state = (Statement) connect.createStatement();
+            ResultSet resultSet = ((java.sql.Statement) state).executeQuery("select * from users");
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("username"));
+            }
+
+        }catch (Exception e ) {
+            e.printStackTrace();
+        }
+
+
+
+        }
 
     }
-}
-
 
